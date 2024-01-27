2888e98b84df2d6404b8c69f54b8e3c096d20758
Oleksandr Ponomarenko <oon@conscensia.com>
2024-01-28 01:40:28 +0200

    fixed Tailwind issue

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const LinkButton = ({ path, name, color }) => (
-  <Link
-    to={path}
-    className={`bg-${color}-500 hover:bg-${color}-700 text-white py-1 px-2 rounded flex items-center`}>
-    {name}
-  </Link>
-);
+const LinkButton = ({ path, name, color }) => {
+  // Need to do this as appearantly Tailwind does not support dynamic styles
+  const style =
+    color === 'blue'
+      ? 'bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded flex items-center'
+      : 'bg-green-500 hover:bg-green-700 text-white py-1 px-2 rounded flex items-center';
+
+  return (
+    <Link to={path} className={style}>
+      {name}
+    </Link>
+  );
+};
 
 export default LinkButton;
